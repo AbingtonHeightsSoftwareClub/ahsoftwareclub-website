6955b4e16035dcc3f7055c6bce2b033adc974794
William Mulligan <mulliganw26@hsnet.ahsd.org>
2025-10-29 19:08:05 -0400

    Reorganize paths in chatroom/urls.py so that choose_chatroom actually renders

--- ahsoftwareclub/chatroom/urls.py
+++ ahsoftwareclub/chatroom/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:room_name>", views.chatroom, name="chatroom"),
     path("choose_chatroom", views.choose_chatroom, name="choose_chatroom"),
+    path("<str:room_name>", views.chatroom, name="chatroom"),
 ]
