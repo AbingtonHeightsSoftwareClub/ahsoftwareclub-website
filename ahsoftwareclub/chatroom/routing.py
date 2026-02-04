23eb6e429b093f07c8da8eef50ce0e9aaae7d159
Seamus Allsion <smallison3409@gmail.com>
2026-02-03 19:54:49 -0500

    Fixed chatroom

--- ahsoftwareclub/chatroom/routing.py
+++ ahsoftwareclub/chatroom/routing.py
@@ -2,6 +2,16 @@ from django.urls import re_path
 
 from . import consumers
 
+"""
+    re_path is a regular expression matching a URL pattern
+
+    Websockets paths can be weird, so we need regular expression to better describe the url
+
+    The following path means if you have a url ahsoftware.club/ws/chat/<room_name>
+    Send the websocket the room name value was a kwarg (keyword argument--like a dictionary)
+    That is what is in the scope value
+
+    """
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
