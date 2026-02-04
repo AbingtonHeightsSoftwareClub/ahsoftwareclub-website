95a71a47736c626b7bfe95d333756b2c9b4e4c2b
Seamus Allison <121757196+TankTheTortoise@users.noreply.github.com>
2026-02-04 08:34:54 -0500

    Add message input handling for chatroom

--- ahsoftwareclub/chatroom/static/chatroom/chatroom.js
+++ ahsoftwareclub/chatroom/static/chatroom/chatroom.js
@@ -14,3 +14,18 @@ chatSocket.onmessage = function(event){
     const data = JSON.parse(event.data);
     document.querySelector("#chat-log").value += (data.message + '\n');
 }
+document.querySelector('#chat-message-input').focus();
+        document.querySelector('#chat-message-input').onkeyup = function(e) {
+            if (e.key === 'Enter') {  // enter, return
+                document.querySelector('#chat-message-submit').click();
+            }
+        };
+
+        document.querySelector('#chat-message-submit').onclick = function(e) {
+            const messageInputDom = document.querySelector('#chat-message-input');
+            const message = messageInputDom.value;
+            chatSocket.send(JSON.stringify({
+                'message': message
+            }));
+            messageInputDom.value = '';
+        };
