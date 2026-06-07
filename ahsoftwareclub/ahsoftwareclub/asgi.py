6b394ce2c107ab0c84665a41ab8125ceb7107c81
William Mulligan <mulliganw26@hsnet.ahsd.org>
2026-06-07 13:24:15 -0400

    re-add authmiddlewarestack

--- ahsoftwareclub/ahsoftwareclub/asgi.py
+++ ahsoftwareclub/ahsoftwareclub/asgi.py
@@ -8,27 +8,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahsoftwareclub.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ahsoftwareclub.settings')
 django_asgi_app = get_asgi_application()
 
-# The urls for all websockets need to be imported so we put them all together
 from chatroom.routing import chatroom_websocket_urlpatterns
 from monopoly.routing import monopoly_websocket_urlpatterns
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket":
-            # Each set of url patterns is a list of routes. We add together each list to combine the lists, which combine the routes
-            URLRouter(chatroom_websocket_urlpatterns + monopoly_websocket_urlpatterns
-        ),
-    }
-)
+combined_routes = chatroom_websocket_urlpatterns + monopoly_websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            combined_routes
+        )
+    ),
+})
